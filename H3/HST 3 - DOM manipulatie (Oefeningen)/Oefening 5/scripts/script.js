c76bef825cf18b3af00dc26d04007fb87d9e5e82
arnedinio <jensdg0604@gmail.com>
2026-02-10 16:47:04 +0100

    Oefening 5 mooier maken en oefening 6 maken

--- H3/HST 3 - DOM manipulatie (Oefeningen)/Oefening 5/scripts/script.js
+++ H3/HST 3 - DOM manipulatie (Oefeningen)/Oefening 5/scripts/script.js
@@ -3,15 +3,14 @@ const resultaatUl = document.getElementById("resultaat");
 let naam = "";
 let itemsResultaat = resultaatUl.children;
 let count = 0;
-while ( naam != "Einde")
+while ( naam !== "Einde")
 {
-    naam = prompt("Naam");
-    let score = parseInt(prompt("Score"));
-    if (naam != "Einde")
+    naam = prompt("Naam:");
+    let score = parseInt(prompt("Score:"));
+    if (naam !== "Einde")
     {
         resultaatUl.appendChild(document.createElement("li"));
         itemsResultaat.item(count).textContent = ("Naam:" + naam + "; Score: " + score);
     }
-
     count += 1;
 }
--- H3/HST 3 - DOM manipulatie (Oefeningen)/Oefening 6/scripts/script.js
+++ H3/HST 3 - DOM manipulatie (Oefeningen)/Oefening 6/scripts/script.js
@@ -1,1 +1,17 @@
 //Oefening 6
+let woorden = [];
+let count = 0;
+let woord = "";
+let WOORDEN = [];
+while (count < 5)
+{
+    woord = prompt("Woord:");
+    count += 1;
+    woorden.push(woord);
+}
+
+woorden.forEach(word => console.log(word, "heeft", word.length, "letters"));
+
+woorden.forEach(word => WOORDEN.push(word.toLocaleUpperCase()));
+
+WOORDEN.forEach(word => console.log(word));
